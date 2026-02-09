a782aacc3efff67f4802445de1b218c8bf8257d6
Joel DeSante <joeldesante@gmail.com>
2026-02-09 01:24:26 -0500

    Note to Jay

--- app/database.py
+++ app/database.py
@@ -11,4 +11,15 @@ pool = ConnectionPool(
     min_size=1,
     max_size=10,
     open=False,  # don't connect until startup
-)
+)   ## This can probably go away!
+
+
+### Models
+
+## models go here Jay!!
+
+## Also Jay, for security reasons (and because fuck numbers) I'd like to use UUIDv4 (built into postgres) for ALL primary keys through the database
+## I am aware taht there are some performance impacts but id prefer to use uuids for the sole fact that they can more readily be exposed to a public facing audience.
+
+## Toodle doo!
+## _ Joel
